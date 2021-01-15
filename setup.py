f63fce840465e5ce988e54ce73ead2e4943ad190
Chris Malek <cmalek@caltech.edu>
2021-01-15 09:14:05 -0800

    Docs

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setup(
     name='django-ldaporm',
     version='1.0.0',
-    description='A Django ORM like interface for ldap objectclasses',
+    description='A Django ORM-like interface for ldap objectclasses',
     long_description=long_description,
     long_description_content_type="text/markdown",
     keywords=['django', 'ldap'],
